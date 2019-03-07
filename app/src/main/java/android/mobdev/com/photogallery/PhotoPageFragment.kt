3655c6231da5fa1ad1a57bcf8f5e186e0158c08b
aleksejkocerev <ko4erev@yandex.ru>
2019-03-07 12:37:08 +0300

    finished with WebView

--- app/src/main/java/android/mobdev/com/photogallery/PhotoPageFragment.kt
+++ app/src/main/java/android/mobdev/com/photogallery/PhotoPageFragment.kt
@@ -1,11 +1,18 @@
 package android.mobdev.com.photogallery
 
+import android.annotation.SuppressLint
 import android.net.Uri
 import android.webkit.WebView
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.webkit.WebChromeClient
+import android.webkit.WebViewClient
+import android.widget.ProgressBar
+import android.support.v7.app.AppCompatActivity
+
+
 
 
 class PhotoPageFragment : VisibleFragment() {
@@ -23,15 +30,37 @@ class PhotoPageFragment : VisibleFragment() {
 
     private var mUri: Uri? = null
     private var mWebView: WebView? = null
+    private var mProgressBar: ProgressBar? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         mUri = arguments?.getParcelable(ARG_URI)
     }
 
+    @SuppressLint("SetJavaScriptEnabled")
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val v = inflater.inflate(R.layout.fragment_photo_page, container, false)
+        mProgressBar = v.findViewById(R.id.progressBar) as ProgressBar
+        mProgressBar?.max = 100 // Значения в диапазоне 0-
+        mWebView?.webChromeClient = object : WebChromeClient() {
+            override fun onProgressChanged(webView: WebView, newProgress: Int) {
+                if (newProgress == 100) {
+                    mProgressBar?.visibility = View.GONE
+                } else {
+                    mProgressBar?.visibility = View.VISIBLE
+                    mProgressBar?.progress = newProgress
+                }
+            }
+            override fun onReceivedTitle(webView: WebView, title: String) {
+                val activity = activity as AppCompatActivity?
+                activity?.supportActionBar?.subtitle = title
+            }
+        }
+
         mWebView = v.findViewById(R.id.webView) as WebView
+        mWebView?.settings?.javaScriptEnabled = true
+        mWebView?.webViewClient = WebViewClient()
+        mWebView?.loadUrl(mUri.toString())
         return v
     }
 }
